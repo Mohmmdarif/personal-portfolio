55090c4a7e10f4c8a1ba003a621e232c83c6fb8a
Mohammad Arif Fadhilah <ariffadhilah124@gmail.com>
2025-02-21 00:42:51 +0700

    [feat]: Hero, About, Experience, and Footer

---
+++ src/mocks/brand.ts
@@ -0,0 +1,17 @@
+export const brand = [
+  {
+    name: "Mohammad",
+    className:
+      "transition-transform duration-300 group-hover:animate-bounce group-hover:delay-0",
+  },
+  {
+    name: "Arif",
+    className:
+      "transition-transform duration-300 group-hover:animate-bounce group-hover:delay-75 text-gray-500",
+  },
+  {
+    name: "F.",
+    className:
+      "transition-transform duration-300 group-hover:animate-bounce group-hover:delay-100 text-gray-500",
+  },
+];
---
+++ src/mocks/testimonials.ts
@@ -0,0 +1,23 @@
+import Me from "@/assets/images/aboutme.jpeg";
+
+export const testimonials = [
+  {
+    quote:
+      "A user interface is like a joke. If you have to explain it, it’s not that good.",
+    name: "Mohammad Arif Fadhilah",
+    designation: "Front-End Web Developer",
+    src: Me,
+  },
+  {
+    src: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1623582854588-d60de57fa33f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1636041293178-808a6762ab39?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1624561172888-ac93c696e10c?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
